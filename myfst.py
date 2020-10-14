55221cca3d8cb35cde92de1dedb93e38ef57b852
weiwangncar <weiwang@ucar.edu>
2020-10-14 13:47:29 -0600

    adding script file 1

---
+++ myfst.py
@@ -0,0 +1,29 @@
+
+# 1.
+#print("Hello, World!")
+
+# Read the data file downloaded from ...
+
+# 2.
+filename = "data/wxobs20170821.txt"
+#datafile = open(filename,'r')
+
+# read the first 4 lines
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+#print(datafile.readline())
+
+#datafile = open(filename,'r')
+#data = datafile.read()
+#datafile.close()
+
+# in place of above 3 lines, one can do
+
+with open(filename, 'r') as datafile:
+    data = datafile.read()
+
+# DEBUG
+# print(type(data))
+# print(data)
+
